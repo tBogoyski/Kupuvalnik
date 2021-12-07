ab967af176de60be493101ccd8bc2754cb20ac42
tBogoyski <tano.bogoyski@protonmail.com>
2021-12-07 23:55:34 +0200

    Fixed some things

--- src/main/java/com/project/kupuvalnik/web/UserController.java
+++ src/main/java/com/project/kupuvalnik/web/UserController.java
@@ -2,7 +2,6 @@ package com.project.kupuvalnik.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserController {
