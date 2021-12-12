d50dbdaf539477fc349c584add8e1ca14437e905
tBogoyski <tano.bogoyski@protonmail.com>
2021-12-12 17:18:07 +0200

    Added gaming category

--- src/main/java/com/project/kupuvalnik/models/entity/enums/OfferCategoryEnum.java
+++ src/main/java/com/project/kupuvalnik/models/entity/enums/OfferCategoryEnum.java
@@ -2,5 +2,5 @@ package com.project.kupuvalnik.models.entity.enums;
 
 public enum OfferCategoryEnum {
     VEHICLES, ELECTRONICS, SPORTS, BOOKS, ANIMALS, HOME, GARDEN, CLOTHES, KIDS,
-    SERVICES, TOOLS, WORK
+    SERVICES, TOOLS, WORK, GAMING
 }
